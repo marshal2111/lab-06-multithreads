4b8d7fb8af202e8b368aa067c71162e184559795
farewelly <zaporozhecilya@gmail.com>
2022-05-22 23:37:13 +0300

    fix

---
+++ tests/tests.cpp
@@ -0,0 +1,11 @@
+// Copyright 2021 Your Name <your_email>
+
+#include <stdexcept>
+#include <gtest/gtest.h>
+
+#include "../include/hash_search.hpp"
+
+//
+TEST(Example, EmptyTest) {
+    EXPECT_THROW(example(), std::runtime_error);
+}
